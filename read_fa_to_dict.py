ae3b263c74c1e20320a01e84dc84e0ac47fc3e31
Shuang Jiang <jiang@Shuangs-MacBook-Pro.local>
2017-05-07 23:53:26 -0700

    add read fa data to dict

---
+++ read_fa_to_dict.py
@@ -0,0 +1,42 @@
+# set the file name
+fname = "all-trnas.fa"
+
+print "read data from file"
+array = []  # odd lines store info, even lines store sequence
+flag = False  # flag is used to indicate this is the second+ line for sequence
+with open(fname, "r") as lines:
+    for line in lines:
+        line = line.strip()
+        if line.startswith('>'):
+            array.append(line)
+            flag = False
+        else:
+            if not flag:
+                array[-1] += " " + line
+                flag = True
+            else:
+                array[-1] += line
+
+# define the dict variable
+rna_data = {"idx": [], "Name": [], "Source": [], "Sequence": []}
+print "get DNA info from array read from file"
+count = 0
+for line in array:
+    line_split = line.split(" ")
+    if 'N' not in line_split[-1] and 'K' not in line_split[-1]:
+        rna_data["idx"].append(count)
+        rna_data["Name"].append(line_split[0].split("-")[-1])
+        rna_data["Source"].append(line_split[0].split(".")[0][1:])
+        rna_data["Sequence"].append(line_split[-1])
+        count += 1
+
+print "map from DNA to RNA"
+dna_rna_map = {"A": "U", "T": "A", "C": "G", "G": "C"}
+for id, dna_seq in enumerate(rna_data["Sequence"]):
+    rna_seq = []
+    for dna in dna_seq:
+        rna_seq.append(dna_rna_map[dna])
+    rna_data["Sequence"][id] = rna_seq
+
+
+print "Finish get rna data from file"
