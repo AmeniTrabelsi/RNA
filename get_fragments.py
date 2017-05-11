f0f9a8ccf9b1fe5ab6e3ba5adb45c006ab73fa8d
Xiaoli <jujuxiao@gmail.com>
2017-05-11 12:25:13 -0700

    get fragments by using G to cut off, and calculate the mz value for every fragments

--- get_fragments.py
+++ get_fragments.py
@@ -12,7 +12,26 @@ iso_map = {'C': [12.0, 13.0033548378],
            'P': [30.97376163],
            'Na': [22.9897692809],
            'K': [38.96370668, 39.96399848, 40.96182576]}
-
+RNA_A = {'C': 5, 'H': 5, 'N': 5}
+RNA_U = {'C': 4, 'H': 4, 'N': 2, 'O': 2}
+RNA_G = {'C': 5, 'H': 5, 'N': 5, 'O': 1}
+RNA_C = {'C': 4, 'H': 5, 'N': 3, 'O': 1}
+RNA_constant = {'C': 5, 'H': 7, 'O': 6, 'P': 1}
+mz_A = 0
+for key, value in RNA_A.items():
+    mz_A = mz_A+value*iso_map[key][0]
+mz_U = 0
+for key, value in RNA_U.items():
+    mz_U = mz_U+value*iso_map[key][0]
+mz_G = 0
+for key, value in RNA_G.items():
+    mz_G = mz_G+value*iso_map[key][0]
+mz_C = 0
+for key, value in RNA_C.items():
+    mz_C = mz_C+value*iso_map[key][0]
+mz_constant = 0
+for key, value in RNA_constant.items():
+    mz_constant = mz_constant+value*iso_map[key][0]
 # Algorithm
 # Create a table called “Fragments”
 # Compute the dictionary called “fragments” storing the related table information
@@ -35,8 +54,38 @@ with con:
 
 # For each rna sequence, decompose into multiple fragments
 all_fragments = {"Name": [], "Source": [], "Fragment": [], "MZ": [], "RNAID": []}
-
-
+separate_by = 'G'
+for line in rawRNA:
+    rna_id = line[0]
+    rna_name = line[1]
+    rna_source = line[2]
+    rna_sequence = line[-1]
+    separate_idx = []
+    fragments = []
+    for i, c in enumerate(rna_sequence):
+        if separate_by == c:
+            separate_idx.append(i)
+    for i, j in enumerate(separate_idx):
+        if i == 0:
+            fragments.append(rna_sequence[:j+1])
+        else:
+            fragments.append(rna_sequence[separate_idx[i-1]+1:separate_idx[i]+1])
+    if len(rna_sequence) >= separate_idx[-1]+1:
+        fragments.append(rna_sequence[separate_idx[-1]+1:])
+    fragments_mz = []
+    for i, temp_frag in enumerate(fragments):
+        no_a = temp_frag.count('A')
+        no_u = temp_frag.count('U')
+        no_g = temp_frag.count('G')
+        no_c = temp_frag.count('C')
+        if i == 0:
+            temp_mz = (no_a+no_u+no_g+no_c)*mz_constant+mz_A*no_a+mz_U*no_u+mz_G*no_g+mz_C*no_c-(no_a+no_u+no_g+no_c)*iso_map['H'][0]+iso_map['P'][0]+3*iso_map['O'][0]
+        elif i == len(fragments)-1:
+            temp_mz = (no_a+no_u+no_g+no_c)*mz_constant+mz_A*no_a+mz_U*no_u+mz_G*no_g+mz_C*no_c-(no_a+no_u+no_g+no_c)*iso_map['H'][0]-iso_map['P'][0]-3*iso_map['O'][0]
+        else:
+            temp_mz = (no_a+no_u+no_g+no_c)*mz_constant+mz_A*no_a+mz_U*no_u+mz_G*no_g+mz_C*no_c-(no_a+no_u+no_g+no_c)*iso_map['H'][0]
+        fragments_mz.append(temp_mz)
+    
 # Store the dictionary data into db
 valuesTranspose = np.ndarray.tolist(np.array(all_fragments.values()).T)
 placeholders = ", ".join(["%s"] * len(all_fragments))
