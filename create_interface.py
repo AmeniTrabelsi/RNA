f7e99368f67cfc015439b57e2ec3d9b727b3f993
Xiaoli <jujuxiao@gmail.com>
2017-06-10 09:39:33 -0700

    modify interface

--- create_interface.py
+++ create_interface.py
@@ -120,4 +120,22 @@ editMenu = Menu(menu)
 menu.add_cascade(label="Edit", menu=editMenu)
 editMenu.add_command(label="Redo", command=doNothing)
 
+# ***** Toolbar ****
+
+toolbar = Frame(root, bg="blue")
+insertButt = Button(toolbar, text="Insert Image", command=doNothing)
+insertButt.pack(side=LEFT, padx=2, pady=2)
+printButt = Button(toolbar, text="Print", command=doNothing)
+printButt.pack(side=LEFT, padx=2, pady=2)
+
+toolbar.pack(side=TOP, fill=X)
+
+# ***** Status bar ****
+
+status = Label(root, text="Preparing to do nothing...", bd=1, relief=SUNKEN, anchor=W)
+status.pack(side=BOTTOM, fill=X)
+
+
+# root.tk.call('tk', 'scaling', 2.0)
 root.mainloop()
+
